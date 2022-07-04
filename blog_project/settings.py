41bd1bf55a6f7c39c1e1d80fbf2e18780bf790d2
OLeksandr Nykolyak <nykolyak.o@gmail.com>
2022-07-04 16:00:28 +0300

    Deploying on Heroku

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#9v2dxs)oy0=@14p86el!ta+)h1w-ryne=0%9=qullt6*nat9^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
